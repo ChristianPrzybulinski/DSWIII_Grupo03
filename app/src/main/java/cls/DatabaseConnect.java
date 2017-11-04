99a0592a68b85f2101874d4484b54cd47af20318
Thales <fortes.thales@gmail.com>
2017-11-03 23:19:40 -0200

    Tela modelo

--- app/src/main/java/cls/DatabaseConnect.java
+++ app/src/main/java/cls/DatabaseConnect.java
@@ -2,8 +2,11 @@ package cls;
 
 import android.util.Log;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * Created by gaelrech on 03/11/17.
@@ -18,7 +21,22 @@ public class DatabaseConnect {
     }
 
     public void teste(){
-        //Log.d(String.valueOf(this.db.getRef), "kkk");
+
+        DatabaseReference db = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference dbInstance = db.child("users").child("christianrp").child("Name");
+
+        dbInstance.addListenerForSingleValueEvent(new ValueEventListener(){
+            @Override
+            public void onDataChange(DataSnapshot   dataSnapshot) {
+                String email = dataSnapshot.getValue(String.class);
+                Log.d("testedaporra", email);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
 }
