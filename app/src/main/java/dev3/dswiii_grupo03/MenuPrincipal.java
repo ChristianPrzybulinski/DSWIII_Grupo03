66965a13c1ea993a3899217b4ba87c69e87110dd
Thales <fortes.thales@gmail.com>
2017-11-08 21:56:22 -0200

    Campo nome versao teste

--- app/src/main/java/dev3/dswiii_grupo03/MenuPrincipal.java
+++ app/src/main/java/dev3/dswiii_grupo03/MenuPrincipal.java
@@ -7,9 +7,23 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+
+import Database.DatabaseListenerCheckBox;
+import Database.DatabaseListenerProfile;
 
 
 public class MenuPrincipal extends ManagerActivity {
+    private String login;
+    private EditText name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +32,19 @@ public class MenuPrincipal extends ManagerActivity {
         setContentView(R.layout.activity_menu_principal);   // referencia a atividade em questão
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);   // identifica a toolbar sendo usada
         setSupportActionBar(myToolbar);
+
+        this.login = getIntent().getStringExtra("login");
+        this.name = (EditText) findViewById(R.id.edit_name);
+        loadProfileInformation();
+        this.name.setText("Bem vindo"+this.name);
     }
+
+
+    protected void loadProfileInformation(){
+        //GET INFORMATIONS
+        dbUser.getUserName(this.login).addListenerForSingleValueEvent(new DatabaseListenerProfile(this.name));
+    }
+
+
+
 }
