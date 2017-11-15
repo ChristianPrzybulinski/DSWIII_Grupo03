26311db599d4dc37a248d0046aaded9b7d43e24b
Thales <fortes.thales@gmail.com>
2017-11-15 18:17:31 -0200

    Campo nome na tela principal

--- app/src/main/java/dev3/dswiii_grupo03/MenuPrincipal.java
+++ app/src/main/java/dev3/dswiii_grupo03/MenuPrincipal.java
@@ -1,23 +1,8 @@
 package dev3.dswiii_grupo03;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.TextView;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.ValueEventListener;
-
-import Database.DatabaseListenerCheckBox;
 import Database.DatabaseListenerProfile;
 
 
@@ -36,7 +21,6 @@ public class MenuPrincipal extends ManagerActivity {
         this.login = getIntent().getStringExtra("login");
         this.name = (EditText) findViewById(R.id.edit_name);
         loadProfileInformation();
-        this.name.setText("Bem vindo"+this.name);
     }
 
 
