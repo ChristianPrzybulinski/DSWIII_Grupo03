9f24e8e64c2b1609016d1222882a92d83d00576b
Gabriel Rech <gabriel.rech@sap.com>
2017-12-08 11:37:06 -0200

    - corrigido menu - Home recriada - faltando somente conexão com as turmas na tela de home

--- app/src/main/java/dev3/dswiii_grupo03/MenuPrincipal.java
+++ app/src/main/java/dev3/dswiii_grupo03/MenuPrincipal.java
@@ -4,11 +4,20 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import Database.DatabaseListenerAdministrador;
 import Database.DatabaseListenerCheckBox;
 import Database.DatabaseListenerProfile;
+import cls.Turma;
 
 
 public class MenuPrincipal extends ManagerActivity {
@@ -21,21 +30,52 @@ public class MenuPrincipal extends ManagerActivity {
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);   // identifica a toolbar sendo usada
         setSupportActionBar(myToolbar);
 
-
-       // this.name = (EditText) findViewById(R.id.edit_name);
+        TextView name = (TextView) findViewById(R.id.text_name);
         //this.administrador = (EditText) findViewById(R.id.edit_tipoUsuario);
 
-        loadProfileInformation();
+        loadProfileInformation(name);
+        getAllTurmas();
     }
 
 
-    protected void loadProfileInformation(){
-        //GET INFORMATIONS
-       // dbUser.getUserName(this.login).addListenerForSingleValueEvent(new DatabaseListenerProfile(this.name));
+    protected void loadProfileInformation(final TextView name){
+        //GET INFORMATION
+        dbUser.getUserName(this.login).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String output = dataSnapshot.getValue(String.class);
+                name.setText(output);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
        // dbUser.getUserAdmin(this.login).addListenerForSingleValueEvent(new DatabaseListenerAdministrador(this.administrador));
 
     }
 
+    protected void getAllTurmas(){
 
+        String dia = "Segunda";
+        final List<Turma> turmas = new ArrayList<>();
+
+        dbTurma.getTurma(dia).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot dsp : dataSnapshot.getChildren()){
+                    turmas.clear();
+                    turmas.add(dsp.getValue(Turma.class));
+                }
+                unShowBar();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
 
 }
